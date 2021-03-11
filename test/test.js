97e1573a4485a1c22ae3af28cf18f2a0beac0ed6
zhangky <iszhangky@163.com>
2021-03-11 16:58:01 +0800

    feature: 支持富文本和其他消息

--- test/test.js
+++ test/test.js
@@ -1,6 +1,24 @@
-const webhook = 'https://open.feishu.cn/';
+const webhook = 'https://open.feishu.cn/open-apis/bot/v2/hook';
 const Rebot = require('../index')({ webhook });
-Rebot.sendText('hello, world').then(data => {
+
+// 普通文本
+/* Rebot.sendText('⁣test').then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+}); */
+
+const message  = {
+    title: '日常提示',
+    texts: [
+        [
+            { tag: 'text', text: '第一行：其余标签请' },
+            { tag: 'a', text: '点击链接', href: 'https://open.feishu.cn/document/ukTMukTMukTM/uMDMxEjLzATMx4yMwETM' }
+        ]
+    ]
+}
+// 富文本
+Rebot.sendRickText(message.title, message.texts, 'en_us').then(data => {
     console.log(data);
 }).catch(err => {
     console.log(err);
